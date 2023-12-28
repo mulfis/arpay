4ec01d93ce8f66ce4936e11f04b6cdf5465a4c4e
mulfis <luthfiassidiq12@gmail.com>
2023-12-28 23:36:20 +0700

    update 21

--- mysite/settings.py
+++ mysite/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = 'django-insecure-kc4^0%&&e$t$b9o$w--lu@pc))@^gheaxy#qmc-(sis(b9=8wg'
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = "xM23hkpKRSPHPzLtahjF"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
