c4417bb519716c30ad755cf909a7996c891b050f
mulfis <luthfiassidiq12@gmail.com>
2024-02-07 14:53:15 +0700

    vercel update

--- website/migrations/0001_initial.py
+++ website/migrations/0001_initial.py
@@ -10,7 +10,7 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        ('auth', '0012_alter_user_first_name_max_length'),
+        ('auth', '0001_initial'),
     ]
 
     operations = [
